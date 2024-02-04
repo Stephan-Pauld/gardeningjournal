17709d1e4e2e1119a0328e66e09557cae3742f4f
Stephan Paul <stephanpauld@gmail.com>
2024-02-03 20:50:40 -0700

    modal logic / mutation working

--- front/src/components/NewSeasonModal.tsx
+++ front/src/components/NewSeasonModal.tsx
@@ -1,25 +1,62 @@
 // NewSeasonModal.tsx
 
 import React from 'react';
+import { useForm, SubmitHandler } from "react-hook-form"
 
 interface NewSeasonModalProps {
   isOpen: boolean;
-  onClose: () => void; // Function to close the modal
+  onClose: () => void;
+  addSeason: (options: {
+    variables: {
+      name: string;
+    };
+  }) => Promise<any>;
 }
 
-export const NewSeasonModal: React.FC<NewSeasonModalProps> = ({ isOpen, onClose }) => {
+type Inputs = {
+  seasonName: string
+}
+
+export const NewSeasonModal: React.FC<NewSeasonModalProps> = ({ isOpen, onClose, addSeason }) => {
   if (!isOpen) return null;
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Inputs>()
+
+  const onSubmit: SubmitHandler<Inputs> = async (data) => {
+    try {
+      const result = await addSeason({
+        variables: {
+          name: data.seasonName,
+        },
+      });
+    } catch (error) {
+      console.error('Mutation error:', error);
+    }
+  }
 
   return (
     <div className="fixed inset-0 bg-black bg-opacity-50 z-50 flex justify-center items-center">
       <div className="bg-white rounded-lg shadow-lg p-[56px]">
         <h2 className="text-lg font-bold ">Season Name</h2>
-        <form action="submit">
+        <form onSubmit={handleSubmit(onSubmit)}>
           <div className="flex flex-col m-2 w-[300px]">
-            <input placeholder="Eg: Spring or Fall Planters" className="border border-gray-400 rounded m-2 p-[10px] w-[100%]" type="text"/>
+            <input
+              {...register("seasonName")}
+              className="border border-gray-400 rounded m-2 p-[10px] w-[100%]"
+              placeholder="Eg: Spring or Fall Planters"
+              type="text"
+            />
+            {errors.seasonName && <span className="bg-red-800">This field is required</span>}
           </div>
           <div className="flex justify-between mx-4 mt-6">
-            <button className="bg-[#74ae57] px-6 py-2 rounded-[3px] font-medium" onClick={onClose}>Submit</button>
+          <input
+            className="bg-[#74ae57] px-6 py-2 rounded-[3px] font-medium"
+            type="submit"
+          />
             <button className="bg-red-500 px-6 py-2 rounded-[3px] font-medium" onClick={onClose}>Cancel</button>
           </div>
         </form>
