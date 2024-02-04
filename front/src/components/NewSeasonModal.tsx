1f1bdef2997b12c816f51c13ab6af06fa8b1d689
Stephan Paul <stephanpauld@gmail.com>
2024-02-03 23:21:12 -0700

    cards and all seasons fetched

--- front/src/components/NewSeasonModal.tsx
+++ front/src/components/NewSeasonModal.tsx
@@ -1,42 +1,20 @@
 // NewSeasonModal.tsx
-
 import React from 'react';
-import { useForm, SubmitHandler } from "react-hook-form"
+import { FieldValues, UseFormHandleSubmit, UseFormRegister, FieldErrors } from 'react-hook-form';
 
 interface NewSeasonModalProps {
   isOpen: boolean;
   onClose: () => void;
-  addSeason: (options: {
-    variables: {
-      name: string;
-    };
-  }) => Promise<any>;
+  register: UseFormRegister<FieldValues>;
+  handleSubmit: UseFormHandleSubmit<FieldValues>;
+  onSubmit: (data: FieldValues) => void;
 }
 
-type Inputs = {
-  seasonName: string
-}
 
-export const NewSeasonModal: React.FC<NewSeasonModalProps> = ({ isOpen, onClose, addSeason }) => {
+
+export const NewSeasonModal: React.FC<NewSeasonModalProps> = ({ isOpen, onClose,register,handleSubmit,onSubmit }) => {
   if (!isOpen) return null;
 
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<Inputs>()
-
-  const onSubmit: SubmitHandler<Inputs> = async (data) => {
-    try {
-      const result = await addSeason({
-        variables: {
-          name: data.seasonName,
-        },
-      });
-    } catch (error) {
-      console.error('Mutation error:', error);
-    }
-  }
 
   return (
     <div className="fixed inset-0 bg-black bg-opacity-50 z-50 flex justify-center items-center">
@@ -50,13 +28,12 @@ export const NewSeasonModal: React.FC<NewSeasonModalProps> = ({ isOpen, onClose,
               placeholder="Eg: Spring or Fall Planters"
               type="text"
             />
-            {errors.seasonName && <span className="bg-red-800">This field is required</span>}
           </div>
           <div className="flex justify-between mx-4 mt-6">
-          <input
-            className="bg-[#74ae57] px-6 py-2 rounded-[3px] font-medium"
-            type="submit"
-          />
+            <input
+              className="bg-[#74ae57] px-6 py-2 rounded-[3px] font-medium"
+              type="submit"
+            />
             <button className="bg-red-500 px-6 py-2 rounded-[3px] font-medium" onClick={onClose}>Cancel</button>
           </div>
         </form>
